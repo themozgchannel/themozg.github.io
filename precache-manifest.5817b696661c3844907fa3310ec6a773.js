e9eac592bcdf970f1183f8920265770eafa12861
themozgchannel <themozgchannel@gmail.com>
2020-06-06 02:01:34 +0300

    Updates

---
+++ precache-manifest.5817b696661c3844907fa3310ec6a773.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad90a063c4da9e32a3de3b4db211e30e",
+    "url": "/flash-arbitrage-watcher/index.html"
+  },
+  {
+    "revision": "3ffa585bf938d5773b72",
+    "url": "/flash-arbitrage-watcher/static/css/main.514d9873.chunk.css"
+  },
+  {
+    "revision": "6d053fb46a3ceb87afe4",
+    "url": "/flash-arbitrage-watcher/static/js/2.229121cf.chunk.js"
+  },
+  {
+    "revision": "b0235c4a03082a633ba67a107e0c1969",
+    "url": "/flash-arbitrage-watcher/static/js/2.229121cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ffa585bf938d5773b72",
+    "url": "/flash-arbitrage-watcher/static/js/main.40ba1ce5.chunk.js"
+  },
+  {
+    "revision": "d244a11a403a8dc4052f",
+    "url": "/flash-arbitrage-watcher/static/js/runtime-main.04536111.js"
+  }
+]);
